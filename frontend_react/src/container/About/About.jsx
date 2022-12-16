8aa541375a17dd79624acb79287b1229eee210fa
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2022-12-16 21:36:11 +0600

    sanity linkup in about section & uploaded dynamic data

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion';
 
-import './About.scss';
 import { images } from '../../constants';
-const abouts = [
-    { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01 },
-    { title: 'Frontend Development', description: 'I am a good frontend developer.', imgUrl: images.about02 },
-    { title: 'Backend Development', description: 'I am a good backend developer.', imgUrl: images.about03 },
-    { title: 'MERN Stack', description: 'I am a good MERN Stack developer.', imgUrl: images.about04 },
-]
+import './About.scss';
+import { urlFor, client } from '../../client';
+
+// const abouts = [
+//     { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01 },
+//     { title: 'Frontend Development', description: 'I am a good frontend developer.', imgUrl: images.about02 },
+//     { title: 'Backend Development', description: 'I am a good backend developer.', imgUrl: images.about03 },
+//     { title: 'MERN Stack', description: 'I am a good MERN Stack developer.', imgUrl: images.about04 },
+// ]
 
 const About = () => {
+    const [abouts, setAbouts] = useState([]);
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+
+        client.fetch(query)
+            .then((data) => setAbouts(data))
+    }, []);
     return (
         <>
             <h2 className='head-text'>I Know that <span>Good Development </span><br /> means <span>Good Business</span>
@@ -24,9 +33,9 @@ const About = () => {
                         className="app__profile-item"
                         key={about.title + index}
                     >
-                        <img src={about.imgUrl} alt={about.title} />
+                        <img src={urlFor(about.imgUrl)} alt={about.title} />
                         <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
-                        <p style={{ marginTop: 10 }}>{about.description}</p>
+                        <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
                     </motion.div>
                 ))}
             </div>
