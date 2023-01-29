f0dd820ba2daa3ea8dc264edbb611de45e8a6616
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2023-01-29 23:30:31 +0600

    add social link

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsTwitter, BsLinkedin, BsGithub } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
+
+
     <div>
-      <BsTwitter />
+      <a href='https://www.linkedin.com/in/sazzadshakh/' target="_blank" rel='noreferrer'><BsLinkedin /></a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href='https://github.com/sazzadshakhbd' rel='noreferrer' target="_blank"><BsGithub /></a>
     </div>
     <div>
-      <BsInstagram />
+      <a href='https://www.facebook.com/sobuzshakhbd/' target="_blank" rel='noreferrer'><FaFacebookF /></a>
+    </div>
+    <div>
+      <a href='https://twitter.com/sazzadshakhbd' target="_blank" rel='noreferrer'><BsTwitter /></a>
     </div>
   </div>
 );
